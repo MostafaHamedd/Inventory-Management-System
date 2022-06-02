29facff0ba85b1e3d6bee8ba54c72288bd235657
Jasmeet Nanhra <jasmeetsingh1599@gmail.com>
2022-06-02 13:09:19 -0500

    v1 create item GUI

---
+++ app/src/main/java/comp3350/ims/presentation/ItemCreate.java
@@ -0,0 +1,15 @@
+package comp3350.ims.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comp3350.ims.R;
+
+public class ItemCreate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_create);
+    }
+}
