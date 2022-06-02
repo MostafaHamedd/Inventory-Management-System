5fd4d64e61a523339b4ab3aeb5902655acf7c03f
Jasmeet Nanhra <jasmeetsingh1599@gmail.com>
2022-06-02 18:10:02 -0500

    Finished the create item GUI

--- app/src/main/java/comp3350/ims/presentation/ItemCreate.java
+++ app/src/main/java/comp3350/ims/presentation/ItemCreate.java
@@ -2,6 +2,8 @@ package comp3350.ims.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import comp3350.ims.R;
 
@@ -11,5 +13,18 @@ public class ItemCreate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_create);
+
+        Spinner spinCategory = findViewById(R.id.spinnerCategory);
+        ArrayAdapter<CharSequence> adapterCategory = ArrayAdapter.createFromResource(this,
+                R.array.categories, android.R.layout.simple_spinner_item);
+        adapterCategory.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinCategory.setAdapter(adapterCategory);
+
+        Spinner spinLocation = findViewById(R.id.spinnerLocation);
+        ArrayAdapter<CharSequence> adapterLocation = ArrayAdapter.createFromResource(this,
+                R.array.locations, android.R.layout.simple_spinner_item);
+        adapterLocation.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinLocation.setAdapter(adapterLocation);
+
     }
 }
