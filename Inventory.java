223bc89cf3e8d13f7235c40d46fee398dc80009f
Tellmore01 <88238579+Tellmore01@users.noreply.github.com>
2022-06-05 21:50:09 -0500

    Inventory class

---
+++ Inventory.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+public class Inventory {
+    private int numofItems ;
+    ArrayList<Item> items = new ArrayList() ;
+    //private int size ;
+
+
+    public Inventory(){
+        numofItems = 0;
+    }
+
+    public int getNumOfItems(){
+        return numofItems ;
+    }
+
+    public boolean addItem(Item newItem){
+        if(!items.contains(newItem)) {
+            items.add(newItem) ;
+            numofItems++ ;
+            return true ;
+        }
+        return false ;
+    }
+
+    public boolean removeItem(Item item){
+        String targetId = item.getId() ;
+        boolean removed = false ;
+        for(int i = 0 ; i < items.size() ; i++) {
+            if(items.get(i).getId().equals(targetId)) {
+                items.remove(i) ;
+                removed = true ;
+                numofItems-- ;
+            }
+        }
+
+        return removed ;
+    }
+
+    public Item getItem(String id){
+        String targetId = id;
+        Item item = null ;
+        for(int i = 0 ; i < items.size() ; i++) {
+            if(items.get(i).getId().equals(targetId)) {
+                item = items.get(i) ;
+            }
+        }
+
+        return item ;
+    }
+
+
+}
