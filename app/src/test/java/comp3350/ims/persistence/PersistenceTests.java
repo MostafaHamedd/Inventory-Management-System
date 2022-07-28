6858a41efacad3b6601cbc444237bbf572aa61a5
Sparx <mchenko9@gmail.com>
2022-07-28 13:05:46 -0500

    added testing stuff

--- app/src/test/java/comp3350/ims/persistence/PersistenceTests.java
+++ app/src/test/java/comp3350/ims/persistence/PersistenceTests.java
@@ -1,18 +1,16 @@
-package comp3350.ims.intergration;
+package comp3350.ims.persistence;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-
-public class IntegrationTests extends TestCase {
+public class PersistenceTests extends TestCase {
     public static TestSuite suite;
 
     public static Test suite()
     {
         suite = new TestSuite("Integration tests");
-        suite.addTestSuite(BusinessPersistenceSeamTest.class);
-        suite.addTestSuite(DataAccessHSQLDBTest.class);
+        suite.addTestSuite(DataAccessTest.class);
         return suite;
     }
 }
